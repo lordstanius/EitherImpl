798cf79f6598467453ae759f713bf8c4ecb93ece
Mladen Stanišić <lordstanius@gmail.com>
2018-04-21 15:59:12 +0200

    Initial

---
+++ Example.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace EitherImpl
+{
+    /// <summary>
+    /// How to consume Result class
+    /// </summary>
+    public class Example
+    {
+        static void Main(string[] args)
+        {
+            Uri address = new Uri("http://www.digger.org/index.html");
+
+            Result<Resource, Error> result = FetchContent(address);
+
+            string content = result.Try(res => res.Data).Catch(err => err.Message);
+            Console.WriteLine(content);
+
+            // or
+            result
+                .Try(res => Console.WriteLine(res.Data))
+                .Catch(err => Console.WriteLine(err.Message));
+        }
+
+        static Result<Resource, Error> FetchContent(Uri address)
+        {
+            // Uncomment line below to see what happens in a case of failure
+            //return Result.Create<Resource, Error>(new NotFound());
+
+            var request = WebRequest.Create(address);
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                if (response.StatusCode == HttpStatusCode.NotFound)
+                {
+                    return Result.Create<Resource, Error>(new NotFound());
+                }
+
+                if (response.StatusCode == HttpStatusCode.Redirect || response.StatusCode == HttpStatusCode.TemporaryRedirect)
+                {
+                    Uri redirectUri = new Uri(response.Headers[HttpResponseHeader.Location]);
+                    return Result.Create<Resource, Error>(new Moved(redirectUri));
+                }
+
+                if (response.StatusCode != HttpStatusCode.OK)
+                {
+                    return Result.Create<Resource, Error>(new Error());
+                }
+
+                Stream dataStream = response.GetResponseStream();
+                string data = new StreamReader(dataStream).ReadToEnd();
+                return Result.Create<Resource, Error>(new Resource(data));
+            }
+            catch (WebException ex) when (ex.Status == WebExceptionStatus.Timeout)
+            {
+                return Result.Create<Resource, Error>(new TimeOut());
+            }
+            catch (WebException ex)
+            {
+                return Result.Create<Resource, Error>(new Error(ex.Message));
+            }
+        }
+
+        /// <summary>
+        /// Base "failure" type
+        /// </summary>
+        class Error
+        {
+            public string Message { get; protected set; }
+
+            public Error()
+            {
+                Message = "Operation failed";
+            }
+
+            public Error(string message) : this()
+            {
+                Message = $"{Message}: {message}";
+            }
+        }
+
+        class NotFound : Error
+        {
+            public NotFound() : base("Resource not found")
+            {
+            }
+        }
+
+        class Moved : Error
+        {
+            public Uri RedirectUri { get; }
+
+            public Moved(Uri redirectUri) : base($"Moved to {redirectUri}")
+            {
+                RedirectUri = redirectUri;
+            }
+        }
+
+        class TimeOut : Error
+        {
+            public TimeOut() : base("Connection timeout")
+            {
+            }
+        }
+
+        /// <summary>
+        /// This is "success" type
+        /// </summary>
+        class Resource
+        {
+            public string Data { get; }
+
+            public Resource(string data)
+            {
+                Data = data;
+            }
+        }
+    }
+}
+
+/*
+This is free and unencumbered software released into the public domain.
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+For more information, please refer to <http://unlicense.org/>
+*/
--- Result.cs
+++ Result.cs
@@ -4,12 +4,16 @@ namespace EitherImpl
 {
     public abstract class Result<TSuccess, TFailure>
     {
-        public abstract Result<TSuccess, TNewSuccess> Handle<TNewSuccess>(Func<TFailure, TNewSuccess> toSuccess);
-        public abstract Result<TNewFailure, TFailure> Handle<TNewFailure>(Func<TSuccess, TNewFailure> toFailure);
-        public abstract TSuccess Reduce(Func<TFailure, TSuccess> mapFailureToSuccess);
-        public abstract TFailure Reduce(Func<TSuccess, TFailure> mapSuccessToFailure);
+        public abstract Result<TResult, TFailure> Try<TResult>(Func<TSuccess, TResult> mapSuccessfullResult);
+        public abstract TSuccess Catch(Func<TFailure, TSuccess> mapFailure);
+
+        public abstract Result<TSuccess, TFailure> Try(Action<TSuccess> doIfSuccess);
+        public abstract void Catch(Action<TFailure> doIfFailure);
     }
 
+    /// <summary>
+    /// Factory class
+    /// </summary>
     public static class Result
     {
         private sealed class Success<TSuccess, TFailure> : Result<TSuccess, TFailure>
@@ -21,30 +25,31 @@ namespace EitherImpl
                 this.value = value;
             }
 
-            public override Result<TSuccess, TNewSuccess> Handle<TNewSuccess>(Func<TFailure, TNewSuccess> toSuccess)
+            public override Result<TResult, TFailure> Try<TResult>(Func<TSuccess, TResult> mapSuccessfullResult)
             {
-                if (toSuccess == null)
-                    throw new ArgumentNullException(nameof(toSuccess));
+                if (mapSuccessfullResult == null)
+                    throw new ArgumentNullException(nameof(mapSuccessfullResult));
 
-                return new Success<TSuccess, TNewSuccess>(value);
+                return new Success<TResult, TFailure>(mapSuccessfullResult(value));
             }
 
-            public override Result<TNewFailure, TFailure> Handle<TNewFailure>(Func<TSuccess, TNewFailure> toFailure)
+            public override Result<TSuccess, TFailure> Try(Action<TSuccess> doIfSuccess)
             {
-                if (toFailure == null)
-                    throw new ArgumentNullException(nameof(toFailure));
+                if (doIfSuccess == null)
+                    throw new ArgumentNullException(nameof(doIfSuccess));
 
-                return new Success<TNewFailure, TFailure>(toFailure(value));
+                doIfSuccess(value);
+
+                return this;
             }
 
-            public override TSuccess Reduce(Func<TFailure, TSuccess> mapFailureToSuccess)
+            public override TSuccess Catch(Func<TFailure, TSuccess> mapFailure)
             {
                 return value;
             }
 
-            public override TFailure Reduce(Func<TSuccess, TFailure> mapSuccessToFailure)
+            public override void Catch(Action<TFailure> doIfFailure)
             {
-                return mapSuccessToFailure(value);
             }
         }
 
@@ -57,30 +62,30 @@ namespace EitherImpl
                 this.value = value;
             }
 
-            public override Result<TSuccess, TNewSuccess> Handle<TNewSuccess>(Func<TFailure, TNewSuccess> toSuccess)
+            public override Result<TResult, TFailure> Try<TResult>(Func<TSuccess, TResult> mapSuccessfullResult)
             {
-                if (toSuccess == null)
-                    throw new ArgumentNullException(nameof(toSuccess));
-
-                return new Failure<TSuccess, TNewSuccess>(toSuccess(value));
+                return new Failure<TResult, TFailure>(value);
             }
 
-            public override Result<TNewFailure, TFailure> Handle<TNewFailure>(Func<TSuccess, TNewFailure> toFailure)
+            public override Result<TSuccess, TFailure> Try(Action<TSuccess> doIfSuccess)
             {
-                if (toFailure == null)
-                    throw new ArgumentNullException(nameof(toFailure));
-
-                return new Failure<TNewFailure, TFailure>(value);
+                return this;
             }
 
-            public override TSuccess Reduce(Func<TFailure, TSuccess> mapFailureToSuccess)
+            public override TSuccess Catch(Func<TFailure, TSuccess> mapFailure)
             {
-                return mapFailureToSuccess(value);
+                if (mapFailure == null)
+                    throw new ArgumentNullException(nameof(mapFailure));
+
+                return mapFailure(value);
             }
 
-            public override TFailure Reduce(Func<TSuccess, TFailure> mapSuccessToFailure)
+            public override void Catch(Action<TFailure> doIfFailure)
             {
-                return value;
+                if (doIfFailure == null)
+                    throw new ArgumentNullException();
+
+                doIfFailure(value);
             }
         }
 
@@ -95,3 +100,26 @@ namespace EitherImpl
         }
     }
 }
+
+/*
+This is free and unencumbered software released into the public domain.
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+For more information, please refer to <http://unlicense.org/>
+*/
